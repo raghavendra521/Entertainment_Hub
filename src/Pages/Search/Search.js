336f1c17de301943434230783af4cfe358ff24e4
BARATAM RAGHAVENDRA <baratamraghavendra@Raghavendras-MacBook-Air.local>
2023-02-20 21:33:31 +0530

    search functionality added

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,16 +1,19 @@
 import { TextField } from "@material-ui/core";
-import { Button, createMuiTheme, Tab, Tabs, ThemeProvider } from "@mui/material";
+import { Button, createTheme, Tab, Tabs, ThemeProvider } from "@mui/material";
 import SearchIcon from "@material-ui/icons/Search"
 import { useEffect, useState } from "react";
 import axios from "axios";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import "../Trending/Trending.css"
 
 const Search = ()=>{
     const [page,setPage]=useState(1);
     const [type,setType]=useState(0);
     const [searchText,setSearchText]=useState("");
     const [numOfPages,setNumOfPages]=useState();
-    const [content,setContent]=useState();
-    const darkTheme = createMuiTheme({
+    const [content,setContent]=useState([]);
+    const darkTheme = createTheme({
         palette:{
             type:"dark",
             primary:{
@@ -20,12 +23,15 @@ const Search = ()=>{
     });
 
     const fetchSearch=async ()=>{
-    const { data } = await axios.get(
-        `https://api.themoviedb.org/3/discover/${type? "tv" : "movie"}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`
-        );
-        console.log("search=>", data);
-        // setMoviesList(data.results);
+        console.log("api=>",`https://api.themoviedb.org/3/search/${type? "tv":"movie"}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchText}`
+        )
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+              process.env.REACT_APP_API_KEY
+            }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+          );
         setContent(data.results)
+        console.log("during search=>", data, content);
         setNumOfPages(data.total_pages)
     }
 
@@ -44,13 +50,19 @@ const Search = ()=>{
                     className="searchBox"
                     label="Search"
                     variant="filled"
+                    onChange={(event)=> {
+                        console.log("event",event);
+                        setSearchText(event.target.value)
+                        }
+                    }   
                     />
-                    <Button variant="contained" style={{marginLeft:10}}>
-                        <SearchIcon />
+                    <Button variant="contained" onClick={fetchSearch} style={{ marginLeft: 10 }}>
+                        <SearchIcon fontSize="large"/>
                     </Button>
                 </div>
 
                 <Tabs value={type} indicatorColor="primary" textColor="primary" onChange={(event,newValue)=>{
+                    console.log("newValue",newValue,event)
                     setType(newValue);
                     setPage(1);
                 }}>
@@ -58,6 +70,21 @@ const Search = ()=>{
                     <Tab style={{width:"50%"}} label="Search TV Series"></Tab>
                 </Tabs>
             </ThemeProvider>
+            <div className="trending">
+            {content &&
+              content.map((c) => (
+                <SingleContent
+                  key={c.id}
+                  id={c.id}
+                  poster={c.poster_path}
+                  title={c.title || c.name}
+                  date={c.first_air_date || c.release_date}
+                  media_type={type ? "tv" : "movie"}
+                  vote_average={c.vote_average}
+                />
+              ))}
+            </div>
+            <CustomPagination setPage={setPage} numOfPages={numOfPages}/>
         </div>
     )
 }
